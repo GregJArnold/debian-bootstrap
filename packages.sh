f8f08e47a302155a6ab8e17c57f5a7cb14512cd4
Greg Arnold <greg@arnoldassociates.com>
2020-09-04 15:35:22 -0400

    First draft of the script to install packages.

---
+++ packages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -qO- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+echo 'deb https://deb.nodesource.com/node_14.x buster main' | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo 'deb-src https://deb.nodesource.com/node_14.x buster main' | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+
+sudo apt-get install -y python3-pip aptitude strace sudo tcpdump nodejs krb5-user
+sudo apt-get purge nano vim-tiny
