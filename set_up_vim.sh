c937a23422e060093a1455297a63e71be9110603
Greg Arnold <greg@arnoldassociates.com>
2020-09-04 15:44:34 -0400

    Create set up vim script.

---
+++ set_up_vim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+ln -s "${DIR}/.vimrc" ~/.vimrc
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+wget -O ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/leafgarland/typescript-vim.git ~/git/typescript-vim
+ln -s "${HOME}/git/typescript-vim" ~/.vim/bundle/typescript-vim
+
+wget -O ~/Downloads/localvimrc.vmb https://www.vim.org/scripts/download_script.php?src_id=27167
+
+vim -c 'so %' -c 'q' ~/Downloads/localvimrc.vmb
