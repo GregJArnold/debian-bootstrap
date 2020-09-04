bd0841674160dc384d1cfb10532591eadf9811da
Greg Arnold <greg@arnoldassociates.com>
2020-09-04 17:39:23 -0400

    NFS instructions.

--- packages.sh
+++ packages.sh
@@ -5,5 +5,5 @@ echo 'deb https://deb.nodesource.com/node_14.x buster main' | sudo tee /etc/apt/
 echo 'deb-src https://deb.nodesource.com/node_14.x buster main' | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 
-sudo apt-get install -y python3-pip aptitude strace sudo tcpdump nodejs krb5-user
+sudo apt-get install -y python3-pip aptitude strace sudo tcpdump nodejs krb5-user nfs-common dnsutils
 sudo apt-get purge nano vim-tiny
