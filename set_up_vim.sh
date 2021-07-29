c02e279a2cf56517f3eb8fdf50d58aeb8a2b1379
Greg Arnold <garnold@ireportsource.com>
2021-07-29 15:12:10 -0400

    Install prettier.

--- set_up_vim.sh
+++ set_up_vim.sh
@@ -12,6 +12,7 @@ ln -s "${HOME}/git/typescript-vim" ~/.vim/bundle/typescript-vim
 
 git clone https://github.com/prettier/vim-prettier ~/git/vim-prettier
 ln -s "${HOME}/git/vim-prettier" ~/.vim/bundle/vim-prettier
+sudo npm i -g prettier
 
 wget -O ~/Downloads/localvimrc.vmb https://www.vim.org/scripts/download_script.php?src_id=27167
 
