a470193d97b04f6cb435e9eb9f29bac2f3b527f0
Greg Arnold <greg@arnoldassociates.com>
2024-03-10 14:36:27 -0400

    Add script for Git commands.

---
+++ git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global alias.dog 'log --all --decorate --oneline --graph'
+git config --global alias.mog 'log --merges --decorate --oneline --graph'
+git config --global grep.lineNumber true
+git config --global http.emptyAuth true
+git config --global pull.ff only
