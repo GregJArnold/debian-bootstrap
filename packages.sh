52d68294db5a1b9d7d466a04787be49ac981d0e2
Greg Arnold <greg@arnoldassociates.com>
2024-03-10 14:28:37 -0400

    Update packages to not install node or Kerberos stuff.

--- packages.sh
+++ packages.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-wget -qO- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-echo 'deb https://deb.nodesource.com/node_14.x buster main' | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo 'deb-src https://deb.nodesource.com/node_14.x buster main' | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 
-sudo apt-get install -y python3-pip aptitude strace sudo tcpdump nodejs krb5-user nfs-common dnsutils vim
-sudo apt-get purge nano vim-tiny
+sudo apt-get install -y --no-install-recommends aptitude strace sudo tcpdump dnsutils vim
+sudo apt-get purge -y nano vim-tiny
