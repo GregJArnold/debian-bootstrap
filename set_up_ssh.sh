082d102c5b63b2ebcf8dfbc4ee572624f062b260
Greg Arnold <greg@arnoldassociates.com>
2024-03-19 14:19:07 -0400

    Add in script to set up SSH.

---
+++ set_up_ssh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+mkdir -m 700 ~/.ssh
+
+ln -s "${DIR}/ssh_config" ~/.ssh/config
+
+mkdir -m 700 ~/.ssh/sockets
