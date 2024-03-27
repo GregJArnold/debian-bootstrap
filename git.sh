618f066b4e3993c504c9973553d7f18f7806e982
Greg Arnold <greg@arnoldassociates.com>
2024-03-27 12:49:45 -0400

    Add in mane/email.

--- git.sh
+++ git.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git config --global user.name 'Greg Arnold'
+git config --global user.email greg@arnoldassociates.com
 git config --global alias.dog 'log --all --decorate --oneline --graph'
 git config --global alias.mog 'log --merges --decorate --oneline --graph'
 git config --global grep.lineNumber true
